77cf9f399215b6833c8093af8edcdcd12dfff81c
Justin Mason <justinmason@justins-macbook-pro-3.local>
2018-11-19 16:03:16 -0500

    Initial Commit

---
+++ notyourbookshelf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notyourbookshelf
+//
+//  Created by Justin Mason on 11/19/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
