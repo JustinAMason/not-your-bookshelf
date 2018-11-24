ea4d4843bdb8454dfc769c907ca7ffbdf1f25e82
Justin Mason <justinmason@172-18-45-35.DYNAPOOL.NYU.EDU>
2018-11-23 19:21:35 -0500

    Add placeholder query to ViewController to demo database query

--- notyourbookshelf/ViewController.swift
+++ notyourbookshelf/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    var db: Firestore!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        db = Firestore.firestore()
+        let settings = db.settings
+        settings.areTimestampsInSnapshotsEnabled = true
+        db.settings = settings
+        
+        db.collection("books").whereField("isbn", isEqualTo: "978-0-321-96760-2").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else if (querySnapshot!.documents.count == 0) {
+                print("Book not found")
+            } else {
+                let isbn = querySnapshot!.documents[0].data()["isbn"] as? String ?? ""
+                let title = querySnapshot!.documents[0].data()["title"] as? String ?? ""
+                let author = querySnapshot!.documents[0].data()["author"] as? String ?? ""
+                
+                print(isbn)
+                print(title)
+                print(author)
+            }
+        }
+        
     }
 
 
